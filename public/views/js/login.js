e187b2ae850fe78fcec5642ed201d0a142b4135c
mahid <mo47da@gmail.com>
2020-07-02 19:07:32 +0600

    login done finally

--- public/views/js/login.js
+++ public/views/js/login.js
@@ -1,5 +1,8 @@
 var btn = document.getElementById("change-btn");
 
+if ( window.history.replaceState ) {
+  window.history.replaceState( null, null, window.location.href );
+}
 
 if (
   localStorage.hasOwnProperty("user-name") &&
@@ -7,84 +10,75 @@ if (
 ) {
   let h = document.getElementById("h-username");
   h.innerHTML =
-    "your current username is " + " " +
+    "your current username is " +
+    " " +
     localStorage.getItem("user-name") +
     ". now go to home page.";
-    btn.style.display = "none";
-    btn.disabled = true;
+  btn.style.display = "none";
+  btn.disabled = true;
   console.log("nofunc");
   //  btn.style.display = "none";
 } else {
-  console.log("elsed")
-  
+  console.log("elsed");
 }
 
+const userExists = async function (elemId) {
+  let element = document.getElementById(elemId).value.trim();
+  let resp = await fetch(`/exists/${element}`);
+  let respData = await resp.json();
+  console.log(respData);
+  return respData
+}
 
-async function checkUser(e) {
+const validUser = function (id) {
+  let userId = document.getElementById(id).value.trim();
+  if (
+    userId === null ||
+    userId === "undefined" ||
+    userId === "null" ||
+    userId.length > 15 ||
+    userId.length < 3
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
+const checkUser = async function (e) {
   if (e) {
     e.preventDefault();
   }
 
-  const validUser = function (id) {
-    let userId = document.getElementById(id).value.trim();
-    if (
-      userId === null ||
-      userId === "undefined" ||
-      userId === "null" ||
-      userId.length > 15 ||
-      userId.length < 3
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  /*
-   function userExists(elementId) {
-    let element = document.getElementById(elementId).value.trim();
-    fetch(`/exists/${element}`)
-      .then((resp) => {
-        return resp.text();
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      });
-  }
-*/
   var id = "input";
   const inputVal = document.getElementById(id).value.trim();
-
+  let resp = await userExists(id);
   if (validUser(id)) {
-    let element = document.getElementById(id).value.trim();
-    let resp = await fetch(`/exists/${element}`);
-    let respData = await resp.json();
-    console.log(respData);
-    console.log("cont");
 
-    if (respData) {
+    if (resp) {
       document.getElementById(id).setCustomValidity("username taken");
       document.getElementById(id).reportValidity();
       console.log("exists");
-      return false;
-    } else {
+     // return false;
+    } else if(!resp){
       document.getElementById(id).setCustomValidity("");
       document.getElementById(id).reportValidity();
-        document.getElementById("loginForm").submit();
+      document.getElementById("loginForm").submit();
       localStorage.setItem("user-name", inputVal);
       let h = document.getElementById("h-username");
-      h.innerHTML = "your username is now " + " " + localStorage.getItem("user-name");
+      h.innerHTML =
+        "your username is now " + " " + localStorage.getItem("user-name");
       document.getElementById("loginForm").submit();
-      return true;
+      document.getElementById(id).disabled = true;
+
+    //  return true;
     }
   } else if (!validUser(id)) {
     document
       .getElementById(id)
       .setCustomValidity("invalid username ,try again");
     document.getElementById(id).reportValidity();
-    return false;
+   // return false;
   }
 }
-
-
